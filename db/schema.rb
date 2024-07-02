403623cf57beb95325ad711c3650dbcdb494960f
Serhii <oserdevit@gmail.com>
2024-07-02 14:57:22 +0300

    accounts added

--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_30_121702) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.uuid "account_id", null: false
+    t.uuid "account_id"
     t.index ["account_id"], name: "index_users_on_account_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
