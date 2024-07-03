41e1fc56e718051d9f0f8a8c344220512f267d48
Serhii <oserdevit@gmail.com>
2024-07-03 14:44:30 +0300

    added managing users

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,11 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.asset_host = "http://localhost:#{ENV['PORT'] || 3000}"
+  config.action_mailer.default_url_options = {
+    :host => "http://localhost:#{ENV['PORT'] || 3000}"
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
